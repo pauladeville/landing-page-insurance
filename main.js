c92eee156ac9589fecc711806af5b1865526cf2f
Paula <paula.deville@posteo.net>
2020-10-22 12:25:54 +0200

    Collapsed nav

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
+const closePanel = () => {
+    document.querySelector("#open-nav > img").setAttribute("src", "images/icon-hamburger.svg");
+    document.getElementById("nav-mobile").classList.replace("active", "hidden");
+}
+
+const openPanel = () => {
+    document.getElementById("nav-mobile").classList.replace("hidden", "active");
+    document.querySelector("#open-nav > img").setAttribute("src", "images/icon-close.svg");
+}
+
 document.getElementById("open-nav").addEventListener("click", () => {
-    console.log("coucou");
-})
+    if(document.getElementById("nav-mobile").classList.contains("active")) {
+        closePanel();
+    } else {
+        openPanel ();
+    }
+});
+
+const navMobileLinks = document.querySelectorAll("#nav-mobile a");
+for (let navMobileLink of navMobileLinks) {
+    navMobileLink.addEventListener("click", () => {
+        closePanel();
+    })
+}
+
+
